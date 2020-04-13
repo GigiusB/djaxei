8b2e51fa84b388fb8c694b9431eaf10deefb0031
Giovanni Bronzini <g.bronzini@gmail.com>
2020-04-13 21:30:25 +0100

    Added ImportException

--- src/djaxei/imp.py
+++ src/djaxei/imp.py
@@ -2,6 +2,8 @@ from collections import OrderedDict
 
 from openpyxl import load_workbook
 
+from djaxei.exceptions import ImportException
+
 
 class Importer:
     def __init__(self, tmpdir=None, **kwargs):
@@ -12,10 +14,14 @@ class Importer:
 
         for ws_name, callback in models_dict.items():
             ws = wb[ws_name]
-            for rownum, row in enumerate(ws.iter_rows(values_only=True)):
+            for rownum, row in enumerate(ws.iter_rows()):
+                row = [cell.value for cell in row]
                 if rownum == 0:
                     header = row
                 else:
-                    callback(OrderedDict(zip(header, row)))
+                    try:
+                        callback(OrderedDict(zip(header, row)))
+                    except Exception as e:
+                        raise ImportException(e, worksheet=ws_name)
         wb.close()
 
